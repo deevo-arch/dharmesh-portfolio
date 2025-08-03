314bfb54294488a1fc0ac97dae29130daabd2a53
Akash Mishra <akashmishrap@icloud.com>
2025-08-04 02:05:02 +0530

    Update Navigation.tsx

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -39,7 +39,7 @@ const Navigation = () => {
         <div className="flex items-center justify-between">
           {/* Logo */}
           <div className="text-2xl font-bold gradient-text">
-            DSG
+            Gopalpur Bihari
           </div>
 
           {/* Desktop Navigation */}
